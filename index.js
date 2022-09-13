b603ee7565c163e184af5d3778a7b24ee6c949ce
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-12 22:54:12 -0300

    Inclusão de texto

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const server = express();
 
+// Query params = ?nome=NodeJS
+// Route Params = /curso/2
+// Request Body = { nome: 'Nodejs', tipo: 'Backend' }
+
 // localhost:3000/curso
 server.get('/curso', (req, res) => {
+  const nome =req.query.nome;
 
-  return res.json({ curso: 'Node JS'});
+  return res.json({ curso: `Aprendendo ${nome}`});
 
 })
 
