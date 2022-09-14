cecf4f502e42bebdfb13447029dc8eeb73d06665
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-13 22:12:28 -0300

    Inclusão de texto

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const server = express();
 // Route Params = /curso/2
 // Request Body = { nome: 'Nodejs', tipo: 'Backend' }
 
-const cursos = ['Node JS', 'JavaScript', 'React Native'];
+//CRUD Creat, Read, Update, Delete
+
+const cursos = ['Node JS', 'Java Script', 'React Native'];
+
+server.get('/cursos', (req, res)=> {
+  return res.json(cursos);
+});
 
 
 // localhost:3000/curso
-server.get('/curso/:index', (req, res) => {
+server.get('/cursos/:index', (req, res) => {
   const { index } =req.params;
 
   return res.json(cursos[index]);
